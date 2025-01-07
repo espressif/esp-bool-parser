1d42a740a6f4e11e80aaf68d7773b18b3fd529a2
igor.udot <horw7001@gmail.com>
2025-01-07 16:22:17 +0800

    fix: add BoolStrm to __all__

--- esp_bool_parser/__init__.py
+++ esp_bool_parser/__init__.py
@@ -9,9 +9,6 @@ Tools for building ESP-IDF related apps.
 
 __version__ = '0.1.1'
 
-from .bool_parser import parse_bool_expr, register_addition_attribute
+from .bool_parser import BoolStmt, parse_bool_expr, register_addition_attribute
 
-__all__ = [
-    'parse_bool_expr',
-    'register_addition_attribute',
-]
+__all__ = ['BoolStmt', 'parse_bool_expr', 'register_addition_attribute']
